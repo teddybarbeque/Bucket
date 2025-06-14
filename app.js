a9e5580c4242101f7893160c67da9a4e241d3129
teddybarbeque <teddybarbeque@gmail.com>
2025-06-14 11:57:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import BucketListApp from "./BucketListApp";
+import BucketListApp from "./BucketListApp.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BucketListApp />);
